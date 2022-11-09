50752840bf654187983d53556a97daab251a90b4
Admin <iamaslonbek@gmail.com>
2022-11-10 00:33:40 +0500

    FileStorageServiceImpl Done

--- src/main/java/com/company/lib/service/file/FileStorageServiceImpl.java
+++ src/main/java/com/company/lib/service/file/FileStorageServiceImpl.java
@@ -7,8 +7,13 @@ import com.company.lib.domains.Uploads;
 import com.company.lib.dto.UploadsDTO;
 import com.company.lib.exceptions.NotFoundException;
 import lombok.NonNull;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 
 import javax.servlet.http.Part;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -99,8 +104,34 @@ public class FileStorageServiceImpl extends AbstractDAO<UploadDAO> implements Fi
     }
 
     @Override
-    public Uploads extractCover(Part book) {
-        return null;
+    public Uploads extractCover(Part part) {
+
+        try {
+            String contentType = "image/png";
+            String[] split = part.getSubmittedFileName().split("\\.");
+            String originalFilename = split[0] + "\\.png";
+            originalFilename = originalFilename.replaceAll(",","_");
+            long size = part.getSize();
+
+            String generatedName = System.currentTimeMillis() + ".png";
+            String path = "/uploads/" + generatedName;
+            Uploads uploads = Uploads.builder()
+                    .contentType(contentType)
+                    .originalName(originalFilename)
+                    .generatedName(generatedName)
+                    .size(size)
+                    .path(path)
+                    .build();
+            String uploadPath = rootPath.resolve(generatedName).toString();
+            dao.save(uploads);
+            PDDocument document = PDDocument.load(part.getInputStream());
+            PDFRenderer pdfRenderer = new PDFRenderer(document);
+            BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(0, 300, ImageType.RGB);
+            ImageIOUtil.writeImage(bufferedImage, uploadPath, 300);
+            return uploads;
+        }catch (IOException e){
+            throw new RuntimeException("Something wrong try again");
+        }
     }
 
     public static FileStorageServiceImpl getInstance(){
