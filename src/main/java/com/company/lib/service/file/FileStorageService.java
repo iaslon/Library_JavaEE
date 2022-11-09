c5888ddc628bf1f92af45e18047496910c1aec27
Admin <iamaslonbek@gmail.com>
2022-11-09 05:15:36 +0500

    FileStorageServiceImpl

--- src/main/java/com/company/lib/service/file/FileStorageService.java
+++ src/main/java/com/company/lib/service/file/FileStorageService.java
@@ -1,9 +1,13 @@
 package com.company.lib.service.file;
 
 import com.company.lib.domains.Uploads;
+import com.company.lib.dto.UploadsDTO;
+import com.company.lib.exceptions.NotFoundException;
+import lombok.NonNull;
 
 import javax.servlet.http.Part;
 import java.io.File;
+import java.util.List;
 
 /**
  * @author "Khazratov Aslonbek"
@@ -14,6 +18,14 @@ public interface FileStorageService {
 
     Uploads upload(Part part);
 
+    File download(@NonNull String fileName);
 
+    UploadsDTO get(@NonNull String fileName);
+
+    List<Uploads> getAll();
+
+    UploadsDTO getByPath(String path) throws NotFoundException;
+
+    Uploads extractCover(Part book);
 
 }
